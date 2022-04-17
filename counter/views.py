79a4b493e419311d7c958935896bea3caaace369
Asif <asifhayatanjon@gmail.com>
2022-04-17 12:46:38 +0600

    new

--- counter/views.py
+++ counter/views.py
@@ -1,3 +1,4 @@
+from cgitb import text
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
@@ -5,5 +6,7 @@ def index(request):
     return render(request, 'index.html')
 
 def counter(request):
-    return render(request,'counter.html')
+    text=request.POST['text']
+    amount_word=len(text.split())
+    return render(request,'counter.html',{'amount':amount_word})
 
